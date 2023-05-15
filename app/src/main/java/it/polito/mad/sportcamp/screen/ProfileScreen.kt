736e2ed103a99f4e32297292ba60b12ed6a9e2d0
SoniaMatranga <soniamatranga1@gmail.com>
2023-05-15 12:41:50 +0200

    fix: ProfileScreen fix

--- app/src/main/java/it/polito/mad/sportcamp/screen/ProfileScreen.kt
+++ app/src/main/java/it/polito/mad/sportcamp/screen/ProfileScreen.kt
@@ -1,309 +1,5 @@
 package it.polito.mad.sportcamp.screen
 
-/*
-import android.content.Context
-import android.widget.Toast
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import it.polito.mad.sportcamp.R
-import it.polito.mad.sportcamp.bottomnav.BottomBarScreen
-import it.polito.mad.sportcamp.ui.theme.SportCampTheme
-
-
-
-
-@Composable
-fun ProfileScreen() {
-    
-    Column(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        SportCampTheme() {
-            //ProfileImage()
-            /*Surface (modifier = Modifier.fillMaxSize()) {
-                UserDetailsList(mutableListOf(
-                    DetailContent("Reservation name 1", "Info \n Info2 \n Info 3"),
-                    DetailContent("Reservation name 2", "Info"),
-                    DetailContent("Reservation name 3", "Info"),
-                    DetailContent("Reservation name 4", "Info"),
-                    DetailContent("Reservation name 4", "Info"),
-                    DetailContent("Reservation name 4", "Info"),
-                    DetailContent("Reservation name 4", "Info"),
-                    DetailContent("Reservation name 4", "Info"),
-
-                ))
-            }*/
-            // Show the options
-            LazyColumn(
-                modifier = Modifier
-                    .fillMaxSize()
-            ) {
-
-                item {
-                    // User's image, name, email and edit button
-                    ProfileImage()
-                }
-
-                // Show the options
-                items(optionsList) { item ->
-                    OptionsItemStyle(item = item, context= LocalContext.current.applicationContext)
-                }
-
-            }
-        }
-    }
-}
-
-data class DetailContent(val field: String, val info: String)
-private val optionsList: ArrayList<OptionsData> = ArrayList()
-
-@Composable
-fun UserDetailsList(details: List<DetailContent>) {
-    LazyColumn {
-        items(details) { detailContent ->
-            DetailsCard(detailContent)
-        }
-    }
-}
-
-@Composable
-fun DetailsCard(content: DetailContent) {
-
-
-    Row(modifier = Modifier.padding(all = 8.dp)) {
-        Image(
-            painter = painterResource(R.drawable.sport_camp_logo),
-            contentDescription = "Court picture",
-            modifier = Modifier
-                // Set image size to 40 dp
-                .size(40.dp)
-                // Clip image to be shaped as a circle
-                .clip(CircleShape)
-                .border(1.5.dp, MaterialTheme.colors.primary, CircleShape)
-        )
-        // Add a horizontal space between the image and the column
-
-
-        Spacer(modifier = Modifier.width(10.dp))
-        Column(modifier = Modifier.fillMaxWidth()) {
-
-            Text(text = content.field,
-                modifier = Modifier.fillMaxWidth())
-            Spacer(modifier = Modifier.height(4.dp))
-            Surface(shape = MaterialTheme.shapes.medium, elevation = 5.dp,
-                // animateContentSize will change the Surface size gradually
-                modifier = Modifier
-                    .animateContentSize()
-                    .padding(1.dp)) {
-                Text(
-                    text = content.info,
-                    style = MaterialTheme.typography.body1,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(all = 4.dp),
-                    // If the message is expanded, we display all its content
-                    // otherwise we only display the first line
-                    maxLines = 1,
-                )
-            }
-
-        }
-    }
-}
-
-@Composable
-fun ProfileImage() {
-
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(all = 8.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ){   Image(
-            painter = painterResource(R.drawable.user_image1),
-            contentDescription = "Profile picture",
-            modifier = Modifier
-                // Clip image to be shaped as a circle
-                .clip(CircleShape)
-                .border(1.5.dp, MaterialTheme.colors.primary, CircleShape)
-        )
-
-    }
-}
-
-// Row style for options
-@Composable
-private fun OptionsItemStyle(item: OptionsData, context: Context) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .clickable(enabled = true) {
-                Toast
-                    .makeText(context, item.title, Toast.LENGTH_SHORT)
-                    .show()
-            }
-            .padding(all = 16.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-
-        // Icon
-        Icon(
-            modifier = Modifier
-                .size(32.dp),
-            imageVector = item.icon,
-            contentDescription = item.title,
-            tint = MaterialTheme.colors.primary
-        )
-
-        Row(
-            modifier = Modifier
-                .fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Column(
-                modifier = Modifier
-                    .weight(weight = 3f, fill = false)
-                    .padding(start = 16.dp)
-            ) {
-
-                // Title
-                Text(
-                    text = item.title,
-                    style = TextStyle(
-                        fontSize = 18.sp,
-                    )
-                )
-
-                Spacer(modifier = Modifier.height(2.dp))
-
-                // Sub title
-                Text(
-                    text = item.subTitle,
-                    style = TextStyle(
-                        fontSize = 14.sp,
-                        letterSpacing = (0.8).sp,
-                        color = Color.Gray
-                    )
-                )
-
-            }
-
-            // Right arrow icon
-            Icon(
-                modifier = Modifier
-                    .weight(weight = 1f, fill = false),
-                imageVector = Icons.Outlined.Notifications,
-                contentDescription = item.title,
-                tint = Color.Black.copy(alpha = 0.70f)
-            )
-        }
-
-    }
-}
-
-private fun prepareOptionsData() {
-
-    val appIcons = Icons.Outlined
-
-    optionsList.add(
-        OptionsData(
-            icon = appIcons.Person,
-            title = "Account",
-            subTitle = "Manage your account"
-        )
-    )
-
-    optionsList.add(
-        OptionsData(
-            icon = appIcons.ShoppingCart,
-            title = "Orders",
-            subTitle = "Orders history"
-        )
-    )
-
-    optionsList.add(
-        OptionsData(
-            icon = appIcons.Notifications,
-            title = "Addresses",
-            subTitle = "Your saved addresses"
-        )
-    )
-
-    optionsList.add(
-        OptionsData(
-            icon = appIcons.Check,
-            title = "Saved Cards",
-            subTitle = "Your saved debit/credit cards"
-        )
-    )
-
-    optionsList.add(
-        OptionsData(
-            icon = appIcons.Settings,
-            title = "Settings",
-            subTitle = "App notification settings"
-        )
-    )
-
-    optionsList.add(
-        OptionsData(
-            icon = appIcons.Check,
-            title = "Help Center",
-            subTitle = "FAQs and customer support"
-        )
-    )
-
-    optionsList.add(
-        OptionsData(
-            icon = appIcons.LocationOn,
-            title = "Offers and Coupons",
-            subTitle = "Offers and coupon codes for you"
-        )
-    )
-
-    optionsList.add(
-        OptionsData(
-            icon = appIcons.FavoriteBorder,
-            title = "Wishlist",
-            subTitle = "Items you saved"
-        )
-    )
-}
-
-data class OptionsData(val icon: ImageVector, val title: String, val subTitle: String)
-
-
-*/
-
 import android.content.Context
 import android.widget.Toast
 import androidx.compose.foundation.*
@@ -454,17 +150,6 @@ private fun UserDetails(user: User) {
                 )
         )
 
-       /* Icon(
-            modifier = Modifier
-                .size(24.dp)
-                .align(Alignment.Center)
-                .graphicsLayer(
-                    translationX = 250f, translationY = 300f
-                ),
-            imageVector = Icons.Filled.PhotoCamera,
-            contentDescription = "Edit Details",
-            tint = MaterialTheme.colors.primary
-        )*/
         IconButton(
             onClick = { /* Azione da eseguire quando il bottone viene cliccato */ },
             modifier = Modifier
@@ -521,35 +206,28 @@ private fun UserDetails(user: User) {
                         )
                     }
 
+                // User's bio
+                user.bio?.let {
+                    Text(
+                        text = it,
+                        style = TextStyle(
+                            fontSize = 14.sp,
+                            color = Color.Gray,
+                            letterSpacing = (0.8).sp
+                        ),
+                        overflow = TextOverflow.Ellipsis
+                    )
+                }
+
                 }
 
                 Spacer(modifier = Modifier.height(2.dp))
-            Column(
-                modifier = Modifier
-                    .weight(weight = 3f, fill = false)
-                    .padding(start = 16.dp)
-            ){
-                // User's bio
-            user.bio?.let {
-                Text(
-                    text = it,
-                    style = TextStyle(
-                        fontSize = 14.sp,
-                        color = Color.Gray,
-                        letterSpacing = (0.8).sp
-                    ),
-                    overflow = TextOverflow.Ellipsis
-                )
-            }
-            }
-        }
-
             // Edit button
             IconButton(
                 modifier = Modifier
                     .weight(weight = 1f, fill = false),
                 onClick = {
-                   // Toast.makeText(context, "Edit Button", Toast.LENGTH_SHORT).show()
+                    // Toast.makeText(context, "Edit Button", Toast.LENGTH_SHORT).show()
                 }) {
                 Icon(
                     modifier = Modifier.size(24.dp),
@@ -558,6 +236,10 @@ private fun UserDetails(user: User) {
                     tint = MaterialTheme.colors.primary
                 )
             }
+
+        }
+
+
 
         }
 
