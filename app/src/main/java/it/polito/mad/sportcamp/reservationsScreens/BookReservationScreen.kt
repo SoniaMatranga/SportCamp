d6c67563677d2f7e94328d4a133a156efff6cf1e
SoniaMatranga <soniamatranga1@gmail.com>
2023-05-30 17:17:22 +0200

    feat: book reservation now works with firebase

--- app/src/main/java/it/polito/mad/sportcamp/reservationsScreens/BookReservationScreen.kt
+++ app/src/main/java/it/polito/mad/sportcamp/reservationsScreens/BookReservationScreen.kt
@@ -1,5 +1,7 @@
 package it.polito.mad.sportcamp.reservationsScreens
 
+import android.content.ContentValues
+import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -15,9 +17,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -31,15 +35,25 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
 import androidx.navigation.NavHostController
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import it.polito.mad.sportcamp.bottomnav.DETAIL_ARGUMENT_KEY
 import it.polito.mad.sportcamp.database.AppViewModel
 import it.polito.mad.sportcamp.common.CustomToolbarWithBackArrow
 import it.polito.mad.sportcamp.bottomnav.DETAIL_ARGUMENT_KEY2
 import it.polito.mad.sportcamp.common.BitmapConverter
+import it.polito.mad.sportcamp.database.Court
+import it.polito.mad.sportcamp.database.Reservation
+import it.polito.mad.sportcamp.database.TimeSlot
 import it.polito.mad.sportcamp.ui.theme.Orange
+import kotlinx.coroutines.tasks.await
 
 
 class BookReservationsViewModel : ViewModel() {
@@ -48,27 +62,134 @@ class BookReservationsViewModel : ViewModel() {
 
     var selectedEquipments by mutableStateOf("Select equipments")
     var selectedTimeSlot by  mutableStateOf("Select time slot")
+
+    private val db = Firebase.firestore
+    private val court = MutableLiveData<Court>()
+    private val timeSlots = MutableLiveData<List<TimeSlot>>()
+
+
+    private fun getTimeSlots() {
+        db.collection("slots")
+            .addSnapshotListener { value, error ->
+                if (error != null) {
+                    Log.w(ContentValues.TAG, "Error getting documents.")
+                }
+                if (value != null) {
+                    val timeSlotsList = mutableListOf<TimeSlot>()
+                    for (doc in value.documents) {
+                        val timeSlot = doc.toObject(TimeSlot::class.java)
+                        if (timeSlot != null) {
+                            timeSlotsList.add(timeSlot)
+                        }
+                    }
+                    timeSlots.value = timeSlotsList
+
+                }
+            }
+    }
+    fun getCourtById(id_court: Int): MutableLiveData<Court> {
+        db.collection("courts")
+            .whereEqualTo("id_court", id_court)
+            .addSnapshotListener { value, error ->
+                if (error != null) {
+                    Log.w(ContentValues.TAG, "Error getting documents.")
+                }
+                if (value != null && !value.isEmpty) {
+                    val courtDocument = value.documents[0]
+                    court.value = courtDocument.toObject(Court::class.java)
+                }
+            }
+        return court
+    }
+
+    suspend fun getAvailableTimeSlots(courtId: Int?, date: String?): MutableLiveData<List<String>> {
+        getTimeSlots()
+        val availableTimeSlots = MutableLiveData<List<String>>()
+
+        val reservationsRef = db.collection("reservations")
+        val query = reservationsRef
+            .whereEqualTo("id_court", courtId)
+            .whereEqualTo("date", date)
+
+        try {
+            val reservationsSnapshot = query.get().await()
+            val reservedTimeSlots = reservationsSnapshot.documents.mapNotNull {
+                it.getLong("id_time_slot")?.toString()
+            }
+
+            timeSlots.value?.let { allTimeSlots ->
+                val availableTimeSlotsList = allTimeSlots
+                    .sortedBy { it.id_time_slot }
+                    .filterNot { reservedTimeSlots.contains(it.id_time_slot.toString()) }
+                    .mapNotNull { it.time_slot }
+
+                availableTimeSlots.value = availableTimeSlotsList
+            }
+        } catch (e: Exception) {
+            Log.w(ContentValues.TAG, "Error getting available time slots", e)
+        }
+
+        return availableTimeSlots
+    }
+    fun addReservation(
+        idUser: Int?,
+        idCourt: Int?,
+        timeSlot: String?,
+        date: String?,
+        equipments: String?,
+        options: String?
+    ) {
+        val reservation = Reservation(
+            id_user = idUser,
+            id_court = idCourt,
+            id_time_slot = getTimeSlotId(timeSlot),
+            date = date,
+            equipments = equipments,
+            options = options
+        )
+
+        db.collection("reservations")
+            .add(reservation)
+            .addOnSuccessListener { documentReference ->
+                Log.d(ContentValues.TAG, "Reservation added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(ContentValues.TAG, "Error adding reservation", e)
+            }
+    }
+
+    private fun getTimeSlotId(timeSlot: String?): Int? {
+        return timeSlots.value?.find { it.time_slot == timeSlot }?.id_time_slot
+    }
+
+    companion object {
+        val factory : ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                BookReservationsViewModel()
+            }
+        }
+    }
 }
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun BookReservationScreen(
     navController: NavHostController,
-    viewModel: AppViewModel = viewModel(factory = AppViewModel.factory)
+    vm: BookReservationsViewModel = viewModel(factory = BookReservationsViewModel.factory)
 ) {
-    val vm: BookReservationsViewModel = viewModel()
 
     val idCourt = navController.currentBackStackEntry?.arguments?.getInt(DETAIL_ARGUMENT_KEY).toString()
     val date = navController.currentBackStackEntry?.arguments?.getString(DETAIL_ARGUMENT_KEY2).toString()
 
 
-    val timeSlots by viewModel.getAvailableTimeSlots(idCourt.toInt(), date).observeAsState()
-    val courtDetails by viewModel.getCourtById(idCourt.toInt()).observeAsState()
+    var timeSlots by remember { mutableStateOf(emptyList<String>()) }
 
-    //var expandedTimeSlot by remember { mutableStateOf(false) }
-    //var expandedEquipments by remember { mutableStateOf(false) }
+    LaunchedEffect(idCourt.toInt(), date) {
+        val availableTimeSlots = vm.getAvailableTimeSlots(idCourt.toInt(), date).value
+        timeSlots = availableTimeSlots ?: emptyList()
+    }
 
-    //var selectedEquipments by remember { mutableStateOf("Select equipments") }
-    //var selectedTimeSlot by remember { mutableStateOf("Select time slot") }
+    val courtDetails by vm.getCourtById(idCourt.toInt()).observeAsState()
+
 
     val equipments = listOf("Not requested", "Requested")
     val bitmap = courtDetails?.image?.let { BitmapConverter.converterStringToBitmap(it) }
@@ -271,8 +392,8 @@ Surface(color=Color.White) {
                         onClick = {
                             if (vm.selectedTimeSlot != "Select time slot" && vm.selectedEquipments != "Select equipments") {
                                 courtDetails?.id_court?.let {
-                                    viewModel.addReservation(
-                                        null, 1,
+                                    vm.addReservation(
+                                         1,
                                         it, vm.selectedTimeSlot, date, vm.selectedEquipments, ""
                                     )
                                 }
